ecf5ea114bfe5dd854c90fd8f74b03da99bc1674
Adam Fast <adam@adamfast.com>
2011-11-13 14:30:28 -0600

    Celery's great, but it's too heavyweight for what I'm trying to do. Disabling auto-install of them for now.

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     version="0.0.1",
     packages=["quakes"],
     description="A simple app to download and display USGS earthquake data",
-    install_requires=['django', 'redis>=2.4.5', 'django-celery', 'python-dateutil', 'psycopg2'],
+#    install_requires=['django', 'redis>=2.4.5', 'django-celery', 'python-dateutil', 'psycopg2'],
+    install_requires=['django', 'python-dateutil', 'psycopg2'],
     classifiers=[
         "Programming Language :: Python",
         "Operating System :: OS Independent",
