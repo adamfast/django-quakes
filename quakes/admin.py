a812808263b7aca7921ecab0a4e051f0bca34943
Adam Fast <adam@adamfast.com>
2011-11-22 18:35:53 -0600

    Display earthquakes ordered in reverse chronological order instead of by PK, which is inconsistent.

--- quakes/admin.py
+++ quakes/admin.py
@@ -4,5 +4,7 @@ from quakes.models import *
 
 class QuakeAdmin(admin.OSMGeoAdmin):
     list_display = ('src', 'eqid', 'version', 'datetime', 'magnitude', 'depth', 'nst', 'region')
+    ordering = ('-datetime',)
+
 
 admin.site.register(Quake, QuakeAdmin)
