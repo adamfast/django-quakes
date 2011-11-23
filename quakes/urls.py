051f2632102be50ce596f8aa639ad6cdb075a633
Adam Fast <adam@adamfast.com>
2011-11-22 18:47:08 -0600

    Connect the quake-displaying view.

--- quakes/urls.py
+++ quakes/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import *
-from quakes.views import earthquakes
+from quakes.views import earthquakes, earthquake_display
 
 
 urlpatterns = patterns('',
     (r'^json/$', earthquakes),
-)
 
+    (r'^', earthquake_display),
+)
