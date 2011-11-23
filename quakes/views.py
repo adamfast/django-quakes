d462e25f1c94898e228ef9a24a1a1057d5cf23d6
Adam Fast <adam@adamfast.com>
2011-11-22 19:35:18 -0600

    Very simple method of retrieving quakes if a check hasn't occurred recently.

--- quakes/views.py
+++ quakes/views.py
@@ -1,6 +1,7 @@
 import datetime
 from datetime import timedelta
 from django.conf import settings
+from django.core.cache import cache
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 #from quakes.decorators import json, gzip
@@ -39,6 +40,20 @@ def earthquake_display(request):
     weekago = datetime.datetime.now() - datetime.timedelta(days=7)
     quakes = Quake.objects.filter(datetime__gte=weekago).order_by('-datetime')
 
+    last_check = cache.get('usgs-poll-last-finished', datetime.datetime(2000, 1, 1))
+    checking = cache.get('usgs-poll-in-progress', False)
+    if not checking:
+        cache.set('usgs-poll-in-progress', True)
+        latest_quake_ago = datetime.datetime.now() - quakes[0].datetime
+        latest_check_ago = datetime.datetime.now() - last_check
+        if latest_quake_ago > datetime.timedelta(minutes=5) and latest_check_ago > datetime.timedelta(minutes=5):
+            from django.core import management
+            management.call_command('load_quakes')
+            cache.set('usgs-poll-last-finished', datetime.datetime.now())
+        cache.delete('usgs-poll-in-progress')
+        checking = False
+
     return render_to_response('earthquakes.html', {
         'object_list': quakes,
+        'checking': checking,
     }, context_instance=RequestContext(request))
